a5b3d863911e50508da5ec5541a1a01ebe7f60bd
Justin Fila <justinfila@pool-216-38-184-99-vpn.ringling.edu>
2017-10-07 18:01:20 -0400

    Initial Commit

---
+++ OpenGLTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLTest
+//
+//  Created by Justin Fila on 10/7/17.
+//  Copyright © 2017 Ringling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLTest
+//
+//  Created by Justin Fila on 10/7/17.
+//  Copyright © 2017 Ringling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
