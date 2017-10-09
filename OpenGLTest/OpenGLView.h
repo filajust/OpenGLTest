c3aebbcd9c0a9baba346d01fe3310b1ac533bef9
Justin Fila <justinfila@admins-MacBook-Pro.local>
2017-10-09 12:22:00 -0400

    Mid tutorial [WIP]

---
+++ OpenGLTest/OpenGLView.h
@@ -0,0 +1,23 @@
+//
+//  OpenGLView.h
+//  OpenGLTest
+//
+//  Created by Justin Fila on 10/7/17.
+//  Copyright © 2017 Ringling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#include <OpenGLES/ES2/gl.h>
+#include <OpenGLES/ES2/glext.h>
+
+@interface OpenGLView : UIView {
+    CAEAGLLayer* _eaglLayer;
+    EAGLContext* _context;
+    GLuint _colorRenderBuffer;
+    
+    GLuint _positionSlot;
+    GLuint _colorSlot;
+}
+
+@end
